c67747b5586c251ab3fc50984603aba7e67b0f39
João Almeida <joao.rafael.almeida@ua.pt>
2022-05-11 15:35:58 +0100

    Add TranSMART mapper

---
+++ src/TranSMARTConstants.py
@@ -0,0 +1,88 @@
+class TranSMARTConstants():
+	#Demographic mappings
+	DemographicSex 			= "2000000609"
+	DemographicRace 		= "2000000603"
+	DemographicEthnic 		= "2000000555"
+	DemographicBirthYear 	= "2000000503"
+	#DemographicCounty 		= 2000000065
+	#DemographicAge 		= 2000000488
+
+	#Observation query index
+	ObservationPersonIdIndex       			= 10#1
+	ObservationObservationConceptIdIndex	= 2
+	ObservationObservationTypeConIdIndex	= 5
+	ObservationValuesAsNumberIndex			= 6
+	ObservationValuesAsStringIndex			= 7
+	ObservationValuesAsConceptIdIndex		= 8
+	ObservationValuesAsConceptNameIndex		= 9
+
+	def observationQuery(schema):
+		return "SELECT observation_id, observation.person_id, observation_concept_id, concept.concept_name, observation_date, \
+       				observation_type_concept_id, value_as_number, value_as_string, value_as_concept_id, \
+       				valueConcept.concept_name,  person.person_source_value \
+	  			FROM "+schema+".observation \
+	  			INNER JOIN "+schema+".concept as concept on "+schema+".observation.observation_concept_id=concept.concept_id \
+				INNER JOIN "+schema+".person as person on "+schema+".observation.person_id=person.person_id \
+	  			LEFT JOIN "+schema+".concept as valueConcept  on "+schema+".observation.value_as_concept_id=valueConcept.concept_id;"
+
+	#Person query index
+	PersonPersonIdIndex       		= 12#0
+	PersonGenderNameIndex       	= 2
+	PersonRaceNameIndex       		= 4
+	PersonEthnicNameIndex       	= 6
+	PersonBirthYearIndex       		= 7
+
+	def personQuery(schema):
+		return "SELECT person_id, gender_concept_id, genderConcept.concept_name as gender_concept_name,\
+	       			race_concept_id, raceConcept.concept_name as race_concept_name,\
+				    ethnicity_concept_id, ethnicityConcept.concept_name as race_concept_name,\
+				    year_of_birth, month_of_birth, day_of_birth, birth_datetime, death_datetime, person_source_value\
+				FROM "+schema+".person\
+				LEFT JOIN "+schema+".concept as genderConcept on "+schema+".person.gender_concept_id = genderConcept.concept_id\
+				LEFT JOIN "+schema+".concept as raceConcept on "+schema+".person.race_concept_id = genderConcept.concept_id\
+				LEFT JOIN "+schema+".concept as ethnicityConcept on "+schema+".person.ethnicity_concept_id = ethnicityConcept.concept_id;"
+	
+
+	Months = {
+		"2100000000":"Baseline",
+		"2100000001":"Month_6",
+		"2100000002":"Month_12",
+		"2100000003":"Month_18",
+		"2100000004":"Month_24",
+		"2100000005":"Month_30",
+		"2100000006":"Month_36",
+		"2100000007":"Month_42",
+		"2100000008":"Month_48",
+		"2100000009":"Month_54",
+		"2100000010":"Month_60",
+		"2100000011":"Month_66",
+		"2100000012":"Month_72",
+		"2100000013":"Month_78",
+		"2100000014":"Month_84",
+		"2100000015":"Month_90",
+		"2100000016":"Month_96",
+		"2100000017":"Month_102",
+		"2100000018":"Month_108",
+		"2100000019":"Month_114",
+		"2100000020":"Month_120",
+		"2100000021":"Month_126",
+		"2100000022":"Month_132",
+		"2100000023":"Month_138",
+		"2100000024":"Month_144",
+		"2100000025":"Month_150",
+		"2100000026":"Month_156",
+		"2100000027":"Month_162",
+		"2100000028":"Month_168",
+		"2100000029":"Month_174",
+		"2100000030":"Month_180",
+		"2100000031":"Month_186",
+		"2100000032":"Month_192",
+		"2100000033":"Month_198",
+		"2100000034":"Month_204",
+		"2100000035":"Month_210",
+		"2100000036":"Month_216",
+		"2100000037":"Month_222",
+		"2100000038":"Month_228",
+		"2100000039":"Month_234",
+		"2100000040":"Month_240"
+	}	
---
+++ src/TranSMARTMap.py
@@ -0,0 +1,255 @@
+from TranSMARTArgs import TranSMARTArgs
+from sqlalchemy import create_engine
+from TranSMARTConstants import TranSMARTConstants
+import pathlib
+
+class TranSMARTMap():
+	def __init__(self, args):
+		self.adHocHarmonization 	= None
+		self.args 					= args
+		self.engine					= create_engine(args.db["datatype"]+"://"+args.db["user"]+":"+args.db["password"]+"@"+args.db["server"]+":"+args.db["port"]+"/"+args.db["database"])
+		self.observationsDict 		= set([TranSMARTConstants.DemographicSex, TranSMARTConstants.DemographicRace, TranSMARTConstants.DemographicEthnic, TranSMARTConstants.DemographicBirthYear])
+		self.visitIndependentCodes 	= []
+		self.visitIndependent		= self.__readVisitIndependent()
+
+	def __readVisitIndependent(self):
+		visitIndependent = []
+		protegeOutputCodes = self.__loadProtegeOutputFileCodes()
+		with open(self.args.vioutput) as fp:
+			for line in fp:
+				visitIndependent += [line.split("\t")[0]]
+				self.visitIndependentCodes += [protegeOutputCodes[line.split("\t")[0]]]
+		return visitIndependent
+
+	def __loadProtegeOutputFileCodes(self):
+		protegeOutput = {}
+		with open(self.args.protegeoutput) as fp:
+			for line in fp:
+				row = line.strip().split("\t")  #['Weight (kg)', 'Clinical_Information+Vital_Signs', '2000000462']
+				protegeOutput[row[0]] = row[2]
+		return protegeOutput
+
+	def setAdHocMethods(self, adHocMethod):
+		self.adHocHarmonization = adHocMethod()
+
+	def createCSV(self):
+		structure = self.__createStructure()
+		tmStructure = self.__buildStructureForTM(structure)
+		preHarmonizedStructure = self.__harmonizeStructureForRM(tmStructure)
+		harmonizedStructure = self.__addAdHocFields(preHarmonizedStructure)
+		self.observationsDict = sorted(list(self.observationsDict))
+		self.__writeCSV(harmonizedStructure)
+		print(self.args.cohortoutputfile + " created")
+
+	def __createStructure(self):
+		newStructure = self.__loadObservations()
+		newStructure = self.__loadPatientData(newStructure)
+		return newStructure
+	
+	def __loadObservations(self):
+		observationSet = self.engine.execute(TranSMARTConstants.observationQuery(self.args.db["schema"]))  
+		newStructure = {}
+		for row in observationSet:
+			person_id = row[TranSMARTConstants.ObservationPersonIdIndex]
+			observation_concept_id = row[TranSMARTConstants.ObservationObservationConceptIdIndex]
+			observation_type_concept_id = row[TranSMARTConstants.ObservationObservationTypeConIdIndex]
+			if str(observation_concept_id) not in self.visitIndependentCodes:
+				obs_id = str(observation_concept_id) + "+" + str(observation_type_concept_id)
+			else:
+				obs_id = str(observation_concept_id)
+
+			if person_id in newStructure:
+				newStructure[person_id][obs_id] = self.__getObservation(row)
+			else:
+				newStructure[person_id] = {}
+				newStructure[person_id][obs_id] = self.__getObservation(row)
+			self.__fulfillObsDict(obs_id)
+		return newStructure
+
+	def __loadPatientData(self, newStructure):
+		personSet = self.engine.execute(TranSMARTConstants.personQuery(self.args.db["schema"]))
+		for row in personSet:  
+			person_id = row[TranSMARTConstants.PersonPersonIdIndex]
+			if person_id not in newStructure:
+				newStructure[person_id] = {}
+			newStructure[person_id][TranSMARTConstants.DemographicSex] 		= self.__convertNoneToEmptyString(row[TranSMARTConstants.PersonGenderNameIndex])
+			newStructure[person_id][TranSMARTConstants.DemographicRace] 	= self.__convertNoneToEmptyString(row[TranSMARTConstants.PersonRaceNameIndex])
+			newStructure[person_id][TranSMARTConstants.DemographicEthnic] 	= self.__convertNoneToEmptyString(row[TranSMARTConstants.PersonEthnicNameIndex])
+			newStructure[person_id][TranSMARTConstants.DemographicBirthYear]= self.__convertNoneToEmptyString(row[TranSMARTConstants.PersonBirthYearIndex])
+		return newStructure
+
+	def __convertNoneToEmptyString(self, value):
+		if value:
+			return value
+		return ""
+
+	def __getObservation(self, row):
+		if row[TranSMARTConstants.ObservationValuesAsConceptNameIndex] != None:
+			return row[TranSMARTConstants.ObservationValuesAsConceptNameIndex]
+		if row[TranSMARTConstants.ObservationValuesAsNumberIndex] != None:
+			return row[TranSMARTConstants.ObservationValuesAsNumberIndex]
+		return row[TranSMARTConstants.ObservationValuesAsStringIndex]
+
+	def __fulfillObsDict(self, observation_concept_id):
+		if observation_concept_id not in self.observationsDict:
+			self.observationsDict.add(str(observation_concept_id))
+
+	def __buildStructureForTM(self, structure):
+		filledCohort = {}
+		for row in structure:
+			filledCohort[row] = structure[row]
+			for obs in self.observationsDict:
+				if obs not in filledCohort[row]:
+					filledCohort[row][obs] = ""
+		return filledCohort
+
+	def __writeCSV(self, tmStructure):
+		header = sorted(list(tmStructure.values())[0])
+		pathlib.Path(self.args.transmartdstdir).mkdir(parents=True, exist_ok=True) 
+		foutput = open('{}{}'.format(self.args.transmartdstdir, self.args.cohortoutputfile), "w")
+		foutput.write("Subject_ID")
+		for colunmn in header:
+			foutput.write("\t" + str(colunmn))
+		foutput.write("\n")
+
+		for row in tmStructure:
+			rowData = sorted(tmStructure[row])
+			patientId = self.__patientIdAdHocMethod(row)
+			foutput.write(str(patientId))
+			for obsID in rowData:
+				foutput.write("\t" + str(tmStructure[row][obsID]))
+			foutput.write("\n")
+		foutput.close()
+
+	def createTMMap(self):
+		#loadProtege_output.txt
+		transmartMapping = {}
+		protegeOutput = self.__loadRowsToMap()
+		self.__writeColumnMap(protegeOutput)
+		self.__createDefaultFiles()
+		print("TranSMART load files created")
+
+	def __loadRowsToMap(self):
+		protegeOutput = {}
+		with open(self.args.protegeoutput) as fp:
+			for line in fp:
+				row = line.strip().split("\t")  #['Weight (kg)', 'Clinical_Information+Vital_Signs', '2000000462']
+				code = row[2]
+				path = row[1]
+				variableName = row[0]
+				if variableName in self.visitIndependent:
+					for column in self.observationsDict:
+						if code == column:
+							print("Independent:", code)
+							if variableName == "Age":
+								protegeOutput[code] = (variableName, self.__buildPath(path, "Baseline"))#path+"+Baseline")
+							else:
+								protegeOutput[code] = (variableName, path)
+				else: #With Baseline or months 
+					for column in self.observationsDict:
+						col = column.split("+")
+						if len(col) == 2:
+							if code == col[0]:
+								protegeOutput[column] = (variableName, self.__buildPath(path, TranSMARTConstants.Months[col[1]]))#path+"+"+TranSMARTConstants.Months[col[1]])
+						else:
+							if code == column:
+								protegeOutput[code] = (variableName, self.__buildPath(path, "Baseline"))#path+"+Baseline")
+		fp.close()
+		return protegeOutput
+
+	def __buildPath(self, path, timer):
+		if "TIMERROOT" in path:
+			return path.replace("TIMERROOT", timer)
+		return "{}+{}".format(path, timer)
+
+	def __buildRow(self, path, columnIndex, variableName):
+		listCategoricalVariables = [
+			"Priority Visuoconstruction",
+			"Smoking Amount",
+			"Alcohol Amount",
+		]
+		for var in listCategoricalVariables:
+			if variableName == var:
+				return self.args.cohortoutputfile + "\t" + path + "\t" + columnIndex + "\t" + variableName + "\t\t\tCATEGORICAL\n"
+		return self.args.cohortoutputfile + "\t" + path + "\t" + columnIndex + "\t" + variableName + "\t\t\t\n"
+
+	def __writeColumnMap(self, protegeOutput):
+		foutput = open('{}{}'.format(self.args.transmartdstdir, "column_map.txt"), "w")
+		foutput.write("Filename\tCategory_Code (tranSMART)\tColumn\tDataLabel\tdata_label_src\tControlVocab_cd\tData_type\n")
+		foutput.write(self.args.cohortoutputfile + "\t\t1\tSUBJ_ID\t\t\t\n")
+		for line in sorted(protegeOutput):
+			row = protegeOutput[line] #['Weight (kg)', 'Clinical_Information+Vital_Signs', '2000000462']
+			columnIndex = str(self.observationsDict.index(line)+2)
+			path = row[1]
+			variableName = row[0]
+			rowToWrite = self.__buildRow(path, columnIndex, variableName)
+			foutput.write(rowToWrite)
+		foutput.close()
+
+	def __createDefaultFiles(self):
+		foutput = open('{}{}'.format(self.args.transmartdstdir, "clinical.params"), "w")
+		foutput.write("COLUMN_MAP_FILE=column_map.txt\nRECORD_EXCLUSION_FILE=x\n")
+		foutput.close()
+		foutput = open('{}{}'.format(self.args.transmartdstdir, "study.params"), "w")
+		foutput.write("STUDY_ID="+self.args.cohortname+"\n")
+		foutput.write("SECURITY_REQUIRED=Y\n")
+		foutput.write("TOP_NODE=\\Private Studies\\"+self.args.cohortname+"\n")
+		foutput.close()
+
+	def __harmonizeStructureForRM(self, tmStructure):
+		#Write here the concepts that need to change due to the TranSMART restrictions
+		#For instance the weight cannot have decimal values 78,4 must be 74
+		harmonizedStructure = {}
+		for row in tmStructure:
+			harmonizedStructure[row] = tmStructure[row]
+			if "2000000462" in tmStructure[row]: #Weight
+				harmonizedStructure[row]["2000000462"] = int(float(tmStructure[row]["2000000462"])) if tmStructure[row]["2000000462"] != '' else ''
+			#...
+			if(self.adHocHarmonization != None):
+				harmonizedStructure[row] = self.__adHocMethods(harmonizedStructure[row])
+
+		return harmonizedStructure
+
+	def __adHocMethods(self, preharmonizedStructure):
+		'''
+		todo: document
+		set_codenumber
+		ex: set_2000000462
+		'''
+		harmonizedStructure = preharmonizedStructure
+		for code in preharmonizedStructure:
+			method = "set_" + str(code)
+			for methodName in dir(self.adHocHarmonization):
+				if methodName in method:
+					if(hasattr(self.adHocHarmonization, methodName)): 
+						harmonizedStructure[code] = getattr(self.adHocHarmonization, methodName)(preharmonizedStructure[code])
+		return harmonizedStructure
+
+	def __addAdHocFields(self, preHarmonizedStructure):
+		harmonizedStructure = preHarmonizedStructure
+		for method in dir(self.adHocHarmonization):
+			if method.startswith("add_"):
+				code = method.split("_")[1]
+				self.observationsDict.add(code)
+				for row in preHarmonizedStructure:
+					harmonizedStructure[row][code] = getattr(self.adHocHarmonization, method)()
+		return harmonizedStructure
+
+	def __patientIdAdHocMethod(self, patientId):
+		for method in dir(self.adHocHarmonization):
+			if method == "patient_id":
+				return getattr(self.adHocHarmonization, method)(patientId)
+		return patientId
+
+
+def main(adHoc=None):
+	argsParsed = TranSMARTArgs.help()
+	args = TranSMARTArgs(argsParsed)
+	tm = TranSMARTMap(args = args)
+	if (adHoc):
+		tm.setAdHocMethods(adHoc)
+	tm.createCSV()
+	tm.createTMMap()
+
+if __name__ == '__main__':
+	main()
